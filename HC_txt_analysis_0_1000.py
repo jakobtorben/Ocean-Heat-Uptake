353c6f8939224b2d3dc9539d223e829c3d68f641
Ben R-W <ben.romero-wilcock17@imperial.ac.uk>
2019-10-29 17:36:07 +0000

    updated txt analysis

--- HC_txt_analysis_0_1000.py
+++ HC_txt_analysis_0_1000.py
@@ -36,7 +36,6 @@ def retrieve(start_year,end_year):
 
 def read_HC(start_year, end_year,depth_from, depth_to):
     n_years = end_year - start_year + 1
-    print(n_years)
     HC = np.ma.zeros([n_years*12, 173, 360])
     year = start_year
     month = 1
@@ -97,18 +96,58 @@ def plot_HC_maps(start_year, depth_from, depth_to, rootgrps, HC_grid):
                 images, duration=0.4)
     
 
+def area(depthtop, lat):
+    re = 6378.137*1e3 #equatorial radius earth (m)
+    rtop = re-depthtop
+    e = 0.08181919  #eccentricity
+    dln = dlt = 1*np.pi/180
+    lt = lat*np.pi/180
+    dA = rtop*rtop*np.cos(lt)*(1-e*e)*dlt*dln/(1-e*e*np.sin(lt)*np.sin(lt))**2    #area
+    return dA
+
+
+def ttl_global_HC(depth_from, HC):
+    ttl_global = np.zeros((len(HC[:,0,0])))
+    for i in range(len(HC[:,0,0])):
+        for lat in range(len(HC[0,:,0])):
+            for lon in range(len(HC[0,0,:])):
+                ttl_global[i] += HC[i,lat,lon]*area(depth_from, lat-83)
+    return ttl_global
+                
+    
+
+def moving_avg(times, HC,s):
+    m = np.zeros((len(HC)-2*s))
+    times = times[s:len(m)+s]
+    for i in range(len(m)):
+        n = i+s
+        m[i] = np.mean(HC[n-s:n+s])
+        
+    return times, m
+
 
 def run(start_year, end_year, depth_from, depth_to):
     years, times, rootgrps = retrieve(start_year, end_year)
     HC = read_HC(start_year, end_year, depth_from, depth_to)
-    HC_global_avg = HC_glob_avg(HC)
-    print(HC_global_avg)
-    plt.plot(times, HC_global_avg)
+#    HC_global_avg = HC_glob_avg(HC)
+#    times1, HC_global_avg = moving_avg(times, HC_global_avg, 12)
+    plt.rcParams['axes.facecolor'] = '#e3e1d8'
+    plt.rcParams['axes.edgecolor'] = '#e3e1d8'
+#    fig1, ax = plt.subplots(figsize=(6.5, 4))
+#    ax.plot(times1, HC_global_avg)
+#    plt.xlabel("Year")
+#    plt.ylabel("Moving Average of (Unweighted) Mean global HC 0m-1000m (Jm^-2)")
+#    plt.grid(color="white")
+#    plt.show()
+    
+    total_global = ttl_global_HC(depth_from, HC)
+    times2, total_global = moving_avg(times, total_global, 12)
+    fig2, ax = plt.subplots(figsize=(6.5, 4))
+    ax.plot(times2, total_global)
     plt.xlabel("Year")
-    plt.ylabel("Mean global HC (Jm^-2)")
+    plt.ylabel("Total global HC 0m-1000m (J)")
     plt.grid(color="white")
     plt.show()
-    #plot(start_year, depth_from, depth_to, rootgrps, HC)
 
 
-run(1950, 1959, 0, 1000)
+run(1950, 1964, 0, 1000)
